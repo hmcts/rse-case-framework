c36861013b23dc182dc167bab348c6221e3d5b7e
Alex McAusland <alex.mcausland@gmail.com>
2020-09-15 09:22:00 +0100

    Obsolete cleandb

--- runLocal.sh
+++ runLocal.sh
@@ -1,6 +1,6 @@
 trap "kill 0" EXIT
 
 export NG_COMMAND='ng serve --host 0.0.0.0 --configuration local'
-./gradlew bootRun & docker-compose up --no-deps --build --abort-on-container-exit db frontend
+./gradlew bootRun & docker-compose up -V --no-deps --build --abort-on-container-exit db frontend
 
 wait
