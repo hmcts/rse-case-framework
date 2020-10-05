556df86286878dbaac14e7d09f1be0ca5fa7168b
banderous <alex.mcausland@gmail.com>
2020-10-05 10:15:53 +0100

    tidy event building (#18)

--- angular/src/app/components/create-event/create-event.component.ts
+++ angular/src/app/components/create-event/create-event.component.ts
@@ -27,67 +27,87 @@ export class CreateEventComponent implements OnInit {
     .event('AddNotes')
       .dynamicPage('Add case notes')
         .question('notes', 'text', 'Enter notes')
-        .build()
-      .build()
+        .buildPage()
+      .buildEvent()
     .event('CloseCase')
       .dynamicPage('Close the case')
         .question('reason', 'text', 'Reason for closure')
-        .build()
-      .build()
+        .buildPage()
+      .buildEvent()
     .event('SubmitAppeal')
       .dynamicPage('Submit an appeal')
         .question('reason', 'text', 'New evidence')
-        .build()
-      .build()
+        .buildPage()
+      .buildEvent()
     .event('CreateClaim')
       .dynamicPage('Claim references')
         .question('claimantReference', 'text', "Claimant\'s legal representative\'s reference", [Validators.required])
         .question('defendantReference', 'text', "Defendant\'s legal representative\'s reference", [Validators.required])
-        .build()
-      .customPage(ChooseCourtComponent, null, DynamicFormAnswersComponent, (x) => {
-        x.title = 'Court location'
-        x.questions = [{ type: 'text', id: 'applicantPreferredCourt', title: 'Applicant\'s preferred court' }]
-      })
-      .customPage(PartyDetailsComponent, null, PartyDetailsAnswersComponent, (x) => {
-        x.title = "Applicant party details"
-      }, 'claimant')
-      .customPage(PartyDetailsComponent, (x) => x.partyType = 'Defendant', PartyDetailsAnswersComponent, (x) => {
-        x.title = 'Defendant party details'
-      }, 'defendant')
-    .build()
+        .buildPage()
+    .customPage(ChooseCourtComponent)
+      .withAnswers(DynamicFormAnswersComponent,(x) => {
+          x.title = 'Court location'
+          x.questions = [{ type: 'text', id: 'applicantPreferredCourt', title: 'Applicant\'s preferred court' }]
+        })
+      .buildPage()
+    .customPage(PartyDetailsComponent)
+      .withAnswers(PartyDetailsAnswersComponent, (x) => {
+          x.title = "Applicant party details"
+        })
+      .withFormGroupName('claimant')
+      .buildPage()
+    .customPage(PartyDetailsComponent)
+      .withInitializer((x) => x.partyType = 'Defendant')
+      .withAnswers(PartyDetailsAnswersComponent, (x) => {
+          x.title = 'Defendant party details'
+        })
+      .withFormGroupName( 'defendant')
+      .buildPage()
+    .buildEvent()
     .event('AddParty')
       .redirectToTab('parties')
-      .customPage(PartyDetailsComponent, (x) => x.partyType = 'Party', PartyDetailsAnswersComponent)
-      .build()
+      .customPage(PartyDetailsComponent)
+      .withInitializer((x) => x.partyType = 'Party')
+      .withAnswers(PartyDetailsAnswersComponent)
+      .buildPage()
+    .buildEvent()
     .event('AddClaim')
       .redirectToTab('claims')
-      .customPage(ChoosePartiesComponent, null, ChoosePartiesAnswersComponent)
-      .customPage(ClaimValueComponent, null, DynamicFormAnswersComponent, (x) => {
-        x.title = 'Claim value'
-        x.questions = [
-          { type: 'currency', id: 'lowerValue', title: 'Claim lower value' },
-        { type: 'currency', id: 'higherValue', title: 'Claim higher value' }
-      ]})
-    .build()
+      .customPage(ChoosePartiesComponent)
+        .withAnswers(ChoosePartiesAnswersComponent)
+      .buildPage()
+      .customPage(ClaimValueComponent)
+      .withAnswers(DynamicFormAnswersComponent, (x) => {
+          x.title = 'Claim value'
+          x.questions = [
+            { type: 'currency', id: 'lowerValue', title: 'Claim lower value' },
+          { type: 'currency', id: 'higherValue', title: 'Claim higher value' }
+        ]})
+      .buildPage()
+    .buildEvent()
     .event('ImportCitizens')
       .redirectToTab('citizens')
-      .customPage(ImportCitizensComponent, null, DynamicFormAnswersComponent, (x) => {
-        x.title = 'Bulk import citizen details'
-        x.questions = [
-          { type: 'text', id: 'fileName', title: 'File name' },
-          { type: 'text', id: 'fileSize', title: 'File size' },
-          ]
-      })
-    .build()
+      .customPage(ImportCitizensComponent)
+      .withAnswers(DynamicFormAnswersComponent, (x) => {
+          x.title = 'Bulk import citizen details'
+          x.questions = [
+            { type: 'text', id: 'fileName', title: 'File name' },
+            { type: 'text', id: 'fileSize', title: 'File size' },
+            ]
+        })
+      .buildPage()
+    .buildEvent()
     .event('PurgeInactiveCitizens')
       .redirectToTab('citizens')
-      .customPage(PurgeInactiveComponent, null, DynamicFormAnswersComponent, (x) => {
-        x.title = 'Purge inactive citizen accounts'
-        x.questions = [
-          { type: 'text', id: 'inactive_count', title: 'Number of inactive accounts' },
-        ]
-      })
-      .build()
+      .customPage(PurgeInactiveComponent)
+      .withAnswers(DynamicFormAnswersComponent, (x) => {
+          x.title = 'Purge inactive citizen accounts'
+          x.questions = [
+            { type: 'text', id: 'inactive_count', title: 'Number of inactive accounts' },
+          ]
+        })
+      .buildPage()
+    .buildEvent()
     .toMap();
 
   private caseId: string;
