0dbed550b1319fad21583412ec297de5f3b69b15
Alex McAusland <alex.mcausland@gmail.com>
2020-11-23 16:43:32 +0000

    wait for db in runLocal.sh

--- runLocal.sh
+++ runLocal.sh
@@ -1,6 +1,8 @@
+set -e
 trap "kill 0" EXIT
 
 export NG_COMMAND='ng serve --host 0.0.0.0 --configuration local'
-./gradlew bootRun & docker-compose up -V --no-deps --build --abort-on-container-exit db frontend
+docker-compose up -V --no-deps --build --abort-on-container-exit db frontend \
+  & ./wait-for-it.sh -t 0 localhost:5432 -- ./gradlew bootRun -i
 
 wait
