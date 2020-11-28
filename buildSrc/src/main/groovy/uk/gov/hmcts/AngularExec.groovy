81ce6c17ee3580b066d7b9751cb94aee65b20ab4
Alex McAusland <alex.mcausland@gmail.com>
2020-11-28 14:00:04 +0000

    Up to date checks for angular tests

---
+++ buildSrc/src/main/groovy/uk/gov/hmcts/AngularExec.groovy
@@ -0,0 +1,21 @@
+package uk.gov.hmcts
+
+
+import org.gradle.api.tasks.Exec
+
+class AngularExec extends Exec {
+    AngularExec() {
+        getInputs().files(project.fileTree(project.projectDir).matching {
+            include "*.json"
+        });
+        getInputs().dir(project.file('src'))
+        workingDir project.getRootDir()
+        commandLine "docker-compose", "run", "--no-deps", "frontend"
+    }
+
+    void ngCommand(String... arguments) {
+        def c = getCommandLine()
+        c.addAll(arguments)
+        setCommandLine(c);
+    }
+}
