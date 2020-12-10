31971bd7cdcbf5da74487bc3690130c1a68bebd7
Alex McAusland <alex.mcausland@gmail.com>
2020-12-10 16:34:56 +0000

    route to parties

--- angular/src/app/case-details/tabs/claims-tab/claims-tab.component.ts
+++ angular/src/app/case-details/tabs/claims-tab/claims-tab.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CaseService} from '../../../services/case-service.service';
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-claims-tab',
--- angular/src/app/case-details/tabs/parties/parties-tab.component.ts
+++ angular/src/app/case-details/tabs/parties/parties-tab.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CaseService} from '../../../services/case-service.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-parties-tab',
@@ -10,13 +11,26 @@ export class PartiesTabComponent implements OnInit {
 
   @Input() caseId: any;
   parties: any = [];
+  party: any;
+  currentTab: string;
   constructor(
     private caseService: CaseService,
+    private route: ActivatedRoute,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
     this.caseService.getCaseParties(this.caseId)
-      .subscribe(x => this.parties = x);
+      .subscribe(x =>  {
+        this.parties = x;
+        this.route.paramMap.subscribe(params => {
+          if (this.parties.length > 0) {
+            const partyId = params.get('entity_id') ?? this.parties[0].party_id;
+            this.currentTab = params.get('case_tab');
+            this.onSelect(partyId);
+          }
+        });
+      });
   }
 
   partyName(party: any) : string {
@@ -26,6 +40,15 @@ export class PartiesTabComponent implements OnInit {
         return party.name
       default:
         return party.title + ' ' + party.firstName + ' ' + party.lastName
+    }
+  }
+
+  onSelect(partyId: any): void {
+    if (partyId) {
+      this.party = this.parties.find(x => x.party_id == partyId);
+      if (this.currentTab === 'parties') {
+        this.router.navigateByUrl(`/cases/${this.caseId}/parties/${partyId}`);
+      }
     }
   }
 }
