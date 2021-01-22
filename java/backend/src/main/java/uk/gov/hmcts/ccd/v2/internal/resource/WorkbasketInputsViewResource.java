f0c5df2b09da934299bfa3d9cfc22ef65a327b64
Alex McAusland <alex.mcausland@gmail.com>
2021-01-22 11:19:33 +0000

    add work basket input

--- java/backend/src/main/java/uk/gov/hmcts/ccd/v2/internal/resource/WorkbasketInputsViewResource.java
+++ java/backend/src/main/java/uk/gov/hmcts/ccd/v2/internal/resource/WorkbasketInputsViewResource.java
@@ -4,19 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.Singular;
 import org.springframework.hateoas.RepresentationModel;
 import uk.gov.hmcts.ccd.domain.model.search.Field;
-import uk.gov.hmcts.ccd.domain.model.search.WorkbasketInput;
-import uk.gov.hmcts.ccd.v2.internal.controller.UIDefinitionController;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
